50161857ad1bdf15d9bef3a96a78e2b39ed2620b
MahmoudHanno <96147168+MahmoudHanno@users.noreply.github.com>
2024-06-11 01:00:04 +0300

    Delete RTL directory

--- RTL/ReLU.sv
+++
@@ -1,8 +0,0 @@
-module RELU #(parameter WIDTH = 8
-			  )
-			 (
-			  input signed [WIDTH-1:0] Data_in,
-			  output [WIDTH-1:0] Data_out
-			 );
-	assign Data_out=(Data_in<0)?0:Data_in[7:0];
-endmodule
